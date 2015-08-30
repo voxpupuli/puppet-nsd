eaea8c8394daffc10c77696acdb39745e41cfa8c
Zach Leslie <zach@puppetlabs.com>
2015-08-30 19:42:05 +0000

    Add FreeBSD support
    Include the necessary params to function on FreeBSD.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,7 +5,7 @@
 class nsd::params {
 
   case $::operatingsystem {
-    'openbsd': {
+    'OpenBSD': {
       $config_file   = '/var/nsd/etc/nsd.conf'
       $zonedir       = '/var/nsd/zones'
       $service_name  = 'nsd'
@@ -13,6 +13,15 @@ class nsd::params {
       $group         = '_nsd'
       $control_cmd   = 'nsd-control'
       $database      = '/var/nsd/db/nsd.db'
+    }
+    'FreeBSD': {
+      $config_file  = '/usr/local/etc/nsd/nsd.conf'
+      $zonedir      = '/usr/local/etc/nsd'
+      $package_name = 'nsd/nsd'
+      $service_name = 'nsd'
+      $owner        = 'nsd'
+      $group        = 'nsd'
+      $database     = '/var/db/nsd/nsd.db'
     }
     default: {
       fail('nsd not supported on this platform, please help add support!')
