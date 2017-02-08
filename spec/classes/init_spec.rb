8a407f2ff11e02db32bcd9568d4091380fd93a97
Zach Leslie <xaque208@gmail.com>
2017-02-07 21:30:45 -0800

    modulesync 2017-02-07

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -3,41 +3,39 @@ require 'spec_helper'
 fact_sets = []
 
 openbsd_facts = {
-  :osfamily => 'OpenBSD',
-  :operatingsystem => 'OpenBSD',
-  :concat_basedir => '/dne',
-  :id => 'root',
-  :kernel => 'OpenBSD',
-  :is_pe => false,
-  :path => "dummy",
+  osfamily: 'OpenBSD',
+  operatingsystem: 'OpenBSD',
+  concat_basedir: '/dne',
+  id: 'root',
+  kernel: 'OpenBSD',
+  is_pe: false,
+  path: 'dummy'
 }
 
 freebsd_facts = {
-  :osfamily => 'FreeBSD',
-  :operatingsystem => 'FreeBSD',
-  :concat_basedir => '/dne',
-  :id => 'root',
-  :kernel => 'FreeBSD',
-  :is_pe => false,
-  :path => "dummy",
+  osfamily: 'FreeBSD',
+  operatingsystem: 'FreeBSD',
+  concat_basedir: '/dne',
+  id: 'root',
+  kernel: 'FreeBSD',
+  is_pe: false,
+  path: 'dummy'
 }
-
 
 fact_sets << openbsd_facts
 fact_sets << freebsd_facts
 
 describe 'nsd' do
   context 'supported operatingsystems' do
-
-    fact_sets.each {|f|
+    fact_sets.each do |f|
       describe "puppet class without any parameters on #{f[:operatingsystem]}" do
         let(:facts) { f }
-        it { should contain_class('nsd') }
-        it { should contain_class('nsd::params') }
-        it { should contain_service('nsd') }
-        it { should contain_exec('nsd-control reconfig') }
-        it { should contain_exec('nsd-control reload') }
+        it { is_expected.to contain_class('nsd') }
+        it { is_expected.to contain_class('nsd::params') }
+        it { is_expected.to contain_service('nsd') }
+        it { is_expected.to contain_exec('nsd-control reconfig') }
+        it { is_expected.to contain_exec('nsd-control reload') }
       end
-    }
+    end
   end
 end
